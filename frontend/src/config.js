a88713f7336cb766c7b96b8746bbe26fc2c4dd70
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-08-25 07:59:35 +0530

    update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 export const USE_MOCK = false;
-export const API_URL = "http://localhost:5000/api";
+//export const API_URL = "http://localhost:5000/api";
+export const API_URL = "https://treasury-app-production-3a69.up.railway.app/api";
 
 export const EMAILJS_SERVICE_ID = "service_28kwr4a";
 export const EMAILJS_TEMPLATE_ID = "template_lgawyhw";
