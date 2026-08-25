78774a4c555fbee087bbae7874b984b374fbf2ec
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-08-25 10:52:40 +0530

    feat: create seed_all_companies.sql

---
+++ backend/sql/seed_all_companies.sql
@@ -0,0 +1,75 @@
+USE railway;
+
+-- CHL
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0101', 'CHL.TeamMember', 'team.chl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CHL', 1),
+('EMP-0102', 'CHL.Accountant', 'accountant.chl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CHL', 1),
+('EMP-0103', 'CHL.CompanyHead', 'head.chl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CHL', 1);
+
+-- cargoserve
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0104', 'cargoserve.TeamMember', 'team.cargoserve@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'cargoserve', 1),
+('EMP-0105', 'cargoserve.Accountant', 'accountant.cargoserve@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'cargoserve', 1),
+('EMP-0106', 'cargoserve.CompanyHead', 'head.cargoserve@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'cargoserve', 1);
+
+-- CMS
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0107', 'CMS.TeamMember', 'team.cms@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CMS', 1),
+('EMP-0108', 'CMS.Accountant', 'accountant.cms@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CMS', 1),
+('EMP-0109', 'CMS.CompanyHead', 'head.cms@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CMS', 1);
+
+-- OCN
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0110', 'OCN.TeamMember', 'team.ocn@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'OCN', 1),
+('EMP-0111', 'OCN.Accountant', 'accountant.ocn@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'OCN', 1),
+('EMP-0112', 'OCN.CompanyHead', 'head.ocn@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'OCN', 1);
+
+-- CSL
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0113', 'CSL.TeamMember', 'team.csl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CSL', 1),
+('EMP-0114', 'CSL.Accountant', 'accountant.csl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CSL', 1),
+('EMP-0115', 'CSL.CompanyHead', 'head.csl2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CSL', 1);
+
+-- CTL
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0116', 'CTL.TeamMember', 'team.ctl@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CTL', 1),
+('EMP-0117', 'CTL.Accountant', 'accountant.ctl@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CTL', 1),
+('EMP-0118', 'CTL.CompanyHead', 'head.ctl@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CTL', 1);
+
+-- CLL
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0119', 'CLL.TeamMember', 'team.cll@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CLL', 1),
+('EMP-0120', 'CLL.Accountant', 'accountant.cll@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CLL', 1),
+('EMP-0121', 'CLL.CompanyHead', 'head.cll@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CLL', 1);
+
+-- CWS
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0122', 'CWS.TeamMember', 'team.cws@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CWS', 1),
+('EMP-0123', 'CWS.Accountant', 'accountant.cws@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CWS', 1),
+('EMP-0124', 'CWS.CompanyHead', 'head.cws@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CWS', 1);
+
+-- Ceyconnect
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0125', 'Ceyconnect.TeamMember', 'team.ceyconnect@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'Ceyconnect', 1),
+('EMP-0126', 'Ceyconnect.Accountant', 'accountant.ceyconnect@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'Ceyconnect', 1),
+('EMP-0127', 'Ceyconnect.CompanyHead', 'head.ceyconnect@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'Ceyconnect', 1);
+
+-- 1MSTS
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0128', 'MSTS.TeamMember', 'team.msts2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', '1MSTS', 1),
+('EMP-0129', 'MSTS.Accountant', 'accountant.msts2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', '1MSTS', 1),
+('EMP-0130', 'MSTS.CompanyHead', 'head.msts2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', '1MSTS', 1);
+
+-- Agencies
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0131', 'Agencies.TeamMember', 'team.agencies@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'Agencies', 1),
+('EMP-0132', 'Agencies.Accountant', 'accountant.agencies@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'Agencies', 1),
+('EMP-0133', 'Agencies.CompanyHead', 'head.agencies@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'Agencies', 1);
+
+123456789
+
+-- CES
+INSERT INTO users (emp_id, name, email, password_hash, role, company, active) VALUES
+('EMP-0134', 'CES.TeamMember', 'team.ces2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TeamMember', 'CES', 1),
+('EMP-0135', 'CES.Accountant', 'accountant.ces2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Accountant', 'CES', 1),
+('EMP-0136', 'CES.CompanyHead', 'head.ces2@chlgroup.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'CompanyHead', 'CES', 1);
